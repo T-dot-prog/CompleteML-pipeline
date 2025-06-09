4714aa444d633353861e3a31e9a4f079e553ff5e
T-dot-prog <tahasinahoni2@gmail.com>
2025-06-09 17:57:53 +0600

    Model evaluation pushed sucessfully

---
+++ src/model_evaluation.py
@@ -0,0 +1,83 @@
+import os
+import numpy as np
+import pandas as pd
+import pickle
+import json
+from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score
+import logging
+import yaml
+from data_ingfestion import logger, load_params, load_data
+
+def load_model(file_path: str):
+    """Load the trained model from a file."""
+    try:
+        with open(file_path, 'rb') as file:
+            model = pickle.load(file)
+        logger.debug('Model loaded from %s', file_path)
+        return model
+    except FileNotFoundError:
+        logger.error('File not found: %s', file_path)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error occurred while loading the model: %s', e)
+        raise
+
+
+def evaluate_model(clf, X_test: np.ndarray, y_test: np.ndarray) -> dict:
+    """Evaluate the model and return the evaluation metrics."""
+    try:
+        y_pred = clf.predict(X_test)
+        y_pred_proba = clf.predict_proba(X_test)[:, 1]
+
+        accuracy = accuracy_score(y_test, y_pred)
+        precision = precision_score(y_test, y_pred)
+        recall = recall_score(y_test, y_pred)
+        auc = roc_auc_score(y_test, y_pred_proba)
+
+        metrics_dict = {
+            'accuracy': accuracy,
+            'precision': precision,
+            'recall': recall,
+            'auc': auc
+        }
+        logger.debug('Model evaluation metrics calculated')
+        return metrics_dict
+    except Exception as e:
+        logger.error('Error during model evaluation: %s', e)
+        raise
+
+
+def save_metrics(metrics: dict, file_path: str) -> None:
+    """Save the evaluation metrics to a JSON file."""
+    try:
+        # Ensure the directory exists
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+        with open(file_path, 'w') as file:
+            json.dump(metrics, file, indent=4)
+        logger.debug('Metrics saved to %s', file_path)
+    except Exception as e:
+        logger.error('Error occurred while saving the metrics: %s', e)
+        raise
+
+
+
+def main():
+    try:
+        params = load_params(params_path='E:/CompleteML-pipeline/params.yaml')
+        clf = load_model('E:/CompleteML-pipeline/models/model.pkl')
+        test_data = load_data('E:/CompleteML-pipeline/data/processed/test_tfidf.csv')
+        
+        X_test = test_data.iloc[:, :-1].values
+        y_test = test_data.iloc[:, -1].values
+
+        metrics = evaluate_model(clf, X_test, y_test)
+
+
+        save_metrics(metrics, 'reports/metrics.json')
+    except Exception as e:
+        logger.error('Failed to complete the model evaluation process: %s', e)
+        print(f"Error: {e}")
+
+if __name__ == '__main__':
+    main()
